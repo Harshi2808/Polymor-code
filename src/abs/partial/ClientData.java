440e49b68e93d4d6bcc699256f799229709ee518
Harshi2808 <kalashri20@gmail.com>
2022-11-20 23:55:26 +0530

    updated

--- src/abs/partial/ClientData.java
+++ src/abs/partial/ClientData.java
@@ -4,6 +4,7 @@ public abstract class ClientData {
 public void clientName() {
 	System.out.println("TCS");
 	System.out.println("T");
+	System.out.println("Tata");
 }
  abstract void clientId(); 
 	 
